a6619d5d24bf09be83aa082f357b0de67b7ee9ec
Jeremie Miller <jeremie@jabber.org>
2010-10-01 19:23:30 -0500

    added nat-detection (useful much later) and initial line type stuff for next step, seeding

--- c/line.c
+++ c/line.c
@@ -1,28 +1,39 @@
-
-typedef struct line_struct
-{
-	char ipp[23]; // IP:PORT
-	char iph[41]; // sha1 of ipp
-	unsigned short ringout;
-	unsigned int seenat;
-	unsigned int sentat;
-	unsigned int lineat;
-	unsigned int br;
-	unsigned int brout;
-	unsigned int brin;
-	unsigned int bsent;
-	short visible;
-	char neighbors[6][41]; // array of nearby iph's
-} *line;
+#include "line.h"
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <openssl/sha.h>
 
 // create+initialize
-line *line_new(char *ipp);
+line line_new(char *ipp, int len)
+{
+	line l;
+	unsigned char sha1bin[20];
+	int i;
+	
+	l = malloc(sizeof(struct line_struct));
+	bzero(l,sizeof(struct line_struct));
+	memcpy(l->ipp,ipp,len);
+	SHA1(ipp, len, sha1bin);
+	for (i = 0; i < 20; i++) sprintf(l->iph+(i*2),"%02x", sha1bin[i]);
+	return l;
+}
 
 // validate incoming line/ring values
-int line_check(line *l, int _line, int _ring);
+int line_check(line l, int _line, int _ring)
+{
+	return 0;
+}
 
 // check+update line byte incoming status
-int line_in(line *l, int _br, int bin);
+int line_in(line l, int _br, int bin)
+{
+	return 0;
+}
 
 // check+update line byte outgoing status
-int line_out(line *l, int bout);
+int line_out(line l, int bout)
+{
+	return 0;
+}
--- c/test_switch.c
+++ c/test_switch.c
@@ -4,10 +4,11 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netdb.h>
-#include <openssl/sha.h>
 #include "telex.h"
 #include "jw.h"
+#include "line.h"
 
 int js0n(unsigned char *js, unsigned int len, unsigned short *out);
 
@@ -18,17 +19,41 @@ main(int argc, char *argv[])
 	struct hostent *ptrh;
 	char *host = "telehash.org";
 	int port = 42424;
-	char buff[2048];
+	char buff[2048],localip[17];
 	int n,i,j;
 	unsigned short js[1024];
-	unsigned char sha1bin[20];
-
+	line self = NULL;
+	
+	// first let's figure out our local IP
 	sock = socket(PF_INET, SOCK_DGRAM, 0);
 	if (sock < 0) {
 		fprintf(stderr, "socket creation failed\n");
 		exit(1);
 	}
- 
+	memset((char *)&sad,0,sizeof(sad));
+	sad.sin_family = AF_INET;
+	sad.sin_port = htons((u_short)port);
+	ptrh = gethostbyname("42.42.42.42"); // dummy just to trick connect to tell us default outgoing IP
+	memcpy(&sad.sin_addr, ptrh->h_addr, ptrh->h_length);
+	connect(sock,(struct sockaddr *)&sad,sizeof(struct sockaddr));
+	i = sizeof(struct sockaddr);
+	if (getsockname(sock, (struct sockaddr *)&sad, &i) == -1)
+	{
+		fprintf(stderr,"getsockname() for local ip lookup failed");
+		exit(1);
+	}
+	sprintf(localip,"%s",inet_ntoa(sad.sin_addr));
+	
+
+	// reset for our real socket
+	close(sock);
+	sock = socket(PF_INET, SOCK_DGRAM, 0);
+	if (sock < 0) {
+		fprintf(stderr, "socket creation failed\n");
+		exit(1);
+	}	
+
+	// our seed
 	memset((char *)&sad,0,sizeof(sad));
 	sad.sin_family = AF_INET;
 	sad.sin_port = htons((u_short)port);
@@ -51,21 +76,24 @@ main(int argc, char *argv[])
 	if(js0n(buff,n,js))
 	{
 		printf("parse failed :(\n");
-	}else{
-		for(i=0;js[i];i+=4)
-		{
-			printf("%.*s\t%.*s\n",js[i+1],buff+js[i],js[i+3],buff+js[i+2]);
-			if(strncmp("_to",buff+js[i],3)==0)
-			{
-				SHA1(buff+js[i+2], js[i+3], sha1bin);
-				printf("SELF: ");
-			    for (i = 0; i < 20; i++) {
-			        printf("%02x", sha1bin[i]);
-			    }
-				printf("\n");
-			}
-		}
+		close(sock);
+		exit(1);
 	}
+
+	for(i=0;js[i];i+=4)
+	{
+		printf("%.*s\t%.*s\n",js[i+1],buff+js[i],js[i+3],buff+js[i+2]);
+		if(strncmp("_to",buff+js[i],3)==0) self = line_new(buff+js[i+2],js[i+3]);
+	}
+	if(!self)
+	{
+		printf("no _to??\n");
+		close(sock);
+		exit(1);
+		
+	}
+	if(self) printf("SELF\tlocal ip %s\tremote ipp %s\tour +end=%s\n",localip,self->ipp,self->iph);
+
 	close(sock);
 }
 
